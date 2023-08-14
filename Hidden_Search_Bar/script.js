61a8676e1939ad5cf6f5f48d09254ae7b188867e
expSaurav <imexpert.saurav@gmail.com>
2023-08-14 17:36:17 +0530

    files added

---
+++ Hidden_Search_Bar/script.js
@@ -0,0 +1,11 @@
+ 
+    let input = document.querySelector('.inputBox');
+    let searchbtn = document.querySelector('.btn');
+
+    searchbtn.addEventListener('click', () => {
+        input.classList.toggle('active');
+       
+        
+        
+    });
+ 
